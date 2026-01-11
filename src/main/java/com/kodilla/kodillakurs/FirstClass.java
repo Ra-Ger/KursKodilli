a90a5b8a8add3c8f635d31995d9d6cda6755031e
Rafał Gerus <gerusrafal@gmail.com>
2026-01-11 10:27:59 +0100

    First commit

---
+++ src/main/java/com/kodilla/kodillakurs/FirstClass.java
@@ -0,0 +1,4 @@
+package com.kodilla.kodillakurs;
+
+public class FirstClass {
+}
---
+++ src/test/java/com/kodilla/kodillakurs/KodillaKursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillakurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaKursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
