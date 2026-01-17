8de4dccc5eb941de43faef8590e822d1c27a531c
Rafał Gerus <gerusrafal@gmail.com>
2026-01-17 13:08:38 +0100

    Test po awarii systemu

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,7 +4,7 @@ public class TestingMain
 {
     public static void main(String[] args)
     {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania...");
     }
 
     public static String vovelsFilter(String s)
