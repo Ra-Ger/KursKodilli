3aece5858a1d2627957e14a933b36f172c914fa2
Rafał Gerus <gerusrafal@gmail.com>
2026-01-18 02:11:39 +0100

    Test kalkulatora

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,15 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain
 {
+
     public static void main(String[] args)
     {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania...");
-    }
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-    public static String vovelsFilter(String s)
-    {
-        return s.replaceAll("(?i)[^aąeęiouóy]","");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int calculatorOutput = calculator.add(27,3);
+        if (calculatorOutput == 30) {
+            System.out.println("dodawanie OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+         calculatorOutput = calculator.subtract(33,3);
+        if (calculatorOutput == 30) {
+            System.out.println("odejmowanie OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
 
