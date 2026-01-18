e2de5e4d86c734b23e25d1d67cb661da64e5aec4
Rafał Gerus <gerusrafal@gmail.com>
2026-01-18 04:33:13 +0100

    6.3 test metody tworzcej liste

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -8,30 +8,6 @@ public class TestingMain
 
     public static void main(String[] args)
     {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-        int calculatorOutput = calculator.add(27,3);
-        if (calculatorOutput == 30) {
-            System.out.println("dodawanie OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-         calculatorOutput = calculator.subtract(33,3);
-        if (calculatorOutput == 30) {
-            System.out.println("odejmowanie OK");
-        } else {
-            System.out.println("Error!");
-        }
 
     }
 }
