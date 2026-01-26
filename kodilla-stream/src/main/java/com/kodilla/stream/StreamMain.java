a18cd212f9505384e7d33e4b692977ce6397ee74
Rafał Gerus <gerusrafal@gmail.com>
2026-01-26 04:31:51 +0100

    7.1 lambda wzbogacacz tekstu

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,41 @@
-package com.kodilla.stream;                                                 // [1]
+package com.kodilla.stream;                                                   // [1]
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;                       // [2]
-import com.kodilla.stream.lambda.Processor;                                 // [3]
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Executor;                                    // [2]
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;                                   // [3]
+import com.kodilla.stream.reference.FunctionalCalculator;
 
-public class StreamMain {                                                   // [4]
+public class StreamMain {                                                     // [4]
 
-    public static void main(String[] args) {                                 // [5]
-        Processor processor = new Processor();                                // [6]
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();  // [7]
-        processor.execute(executeSaySomething);                               // [8]
-    }                                                                        // [9]
-}                                                                           // [10]
+    public static void main(String[] args) {                                   // [5]
+        Processor processor = new Processor();                                  // [6]
+     //   Executor codeToExecute = () -> System.out.println("This is an example text.");  // [7]
+     //   processor.execute(codeToExecute);                                       // [8]
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();       // [5]
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);           // [6]
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);           // [7]
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);           // [8]
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a % b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a % b + (10*b));
+        expressionExecutor.executeExpression(10, 5, (a, b) -> {System.out.println("Zagnieżdżona lambda :D");expressionExecutor.executeExpression(10, 5, (c, e) -> a + b); return a - b;});
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String basicText = "Wyklęty powstań ludu ziemi";
+        System.out.println(basicText);
+        System.out.println(poemBeautifier.beautify(basicText,(str) -> str.toUpperCase()));
+        System.out.println(poemBeautifier.beautify(basicText,(str) -> ":---" + str + "---:"));
+        System.out.println(poemBeautifier.beautify(basicText,(str) -> str.replaceFirst("Wyklęty powstań ludu ziemi","Bolszewickie brednie!!!")));
+        System.out.println(poemBeautifier.beautify(basicText,(str) -> str.replaceAll("[ ]","_")));
+    }
+}
