bb4b2a0bdd58e12fd8aeeae4ed2e36bf506f59bc
Rafał Gerus <gerusrafal@gmail.com>
2026-02-26 04:57:44 +0100

    14.2. Prototype

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class LibraryTestSuite {
+    @Test
+    void testGetBooks()
+    {
+        Library library = new Library("Babilon");
+        Book book1 = new Book("Mein Kampf","Adolf Hitler", LocalDate.of(1925 ,7,18));
+        Book book2 = new Book("101 reasons to cut off your legs","Pedro Mukake", LocalDate.of(2004 ,1,20));
+        Book book3 = new Book("Why Are You Gae?","Simon Kaggwa", LocalDate.of(2012 ,12,18));
+        library.getBooks().add(book1);
+        library.getBooks().add(book2);
+        library.getBooks().add(book3);
+
+        Library libraryShallowCopy = null;
+        Library libraryDeepCopy = null;
+        try {
+           libraryShallowCopy = library.shallowCopy();
+           libraryDeepCopy = library.deepCopy();
+
+        }
+        catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        assertEquals(3, library.getBooks().size());
+        assertNotEquals(true, library.getBooks() == libraryDeepCopy.getBooks());
+        libraryShallowCopy.getBooks().remove(book3);
+        assertEquals(2, libraryShallowCopy.getBooks().size());
+        libraryDeepCopy.getBooks().clear();
+        assertEquals(2, library.getBooks().size());
+    }
+}
