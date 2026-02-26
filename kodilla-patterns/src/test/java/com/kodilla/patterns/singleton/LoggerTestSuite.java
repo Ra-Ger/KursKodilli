a80babc695e499d86096d15d459281c5eb6ea79b
Rafał Gerus <gerusrafal@gmail.com>
2026-02-26 02:49:45 +0100

    14.1. singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    private Logger logger = Logger.INSTANCE;
+
+    @Test
+    void testGetLog() {
+        String log = "Witam";
+        logger.log(log);
+
+        assertEquals(log,logger.getLastLog());
+    }
+}
