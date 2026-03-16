9fe6e2f09f6599e21c75959e16f7432fea692604
Rafał Gerus <gerusrafal@gmail.com>
2026-03-16 01:53:25 +0100

    16.3 Invoice

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,60 @@
+package com.kodilla.hibernate.invoice;
+
+import com.kodilla.hibernate.task.Task;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "INVOICE")
+public class Invoice {
+    private int id;
+    private String number;
+    private List<Item> items;
+
+    public Invoice(String number, List<Item> items) {
+        this.number = number;
+        this.items = items;
+    }
+
+    public Invoice()
+    {
+
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "NUMBER")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @OneToMany(
+            targetEntity = Item.class,
+            mappedBy = "invoice",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import com.kodilla.hibernate.tasklist.dao.TaskListDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    private ProductDao productDao;
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    void testInvoiceDaoSave()
+    {
+        Product product1 = new Product("Not");
+        Product product2 = new Product("Found");
+        Item item1 = new Item(product1, BigDecimal.ONE,9,new BigDecimal(34593));
+        Item item2 = new Item(product2, new BigDecimal(808),300,new BigDecimal(44));
+        List<Item> itemList = new ArrayList<>(List.of(item1,item2));
+        Invoice invoice = new Invoice("404",itemList);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        productDao.save(product1);
+        productDao.save(product2);
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        Invoice findedInvoice = invoiceDao.findById(id).orElseThrow();
+        assertEquals(2,findedInvoice.getItems().size());
+
+        invoiceDao.deleteById(id);
+        productDao.deleteById(product1.getId());
+        productDao.deleteById(product2.getId());
+    }
+}
